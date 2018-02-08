5d698bf31ae60d3c9e6a4a4a9715c457b3911e9d
樊晓龙 <fanxiaolong@gmail.com>
2018-02-08 16:09:17 +0800

    add parcel

---
+++ src/main.js
@@ -0,0 +1,26 @@
+import "../jquery.config.js";
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'animate.css'
+import './css/main.css'
+
+$('#myCarousel').carousel({
+  //设置自动播放/3 秒
+  interval: 300000,
+});
+$(".food-wrap>div").mouseover(function () {
+  if ($(this).hasClass('food-1')) return
+  $(this).children().addClass('show animated rotateInDownRight')
+}).mouseleave(function () {
+  if ($(this).hasClass('food-1')) return
+  $(this).children().removeClass('show animated rotateInDownRight')
+})
+$(".mobile-inner-header-icon").click(function () {
+  $(this).toggleClass("mobile-inner-header-icon-click mobile-inner-header-icon-out");
+  $(".mobile-inner-nav").slideToggle(250);
+});
+$(".mobile-inner-nav a").each(function (index) {
+  $(this).css({
+    'animation-delay': (index / 10) + 's'
+  });
+});
