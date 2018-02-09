4e49e536fb5aba02f3a4a2594ec7db6676f4a5ba
樊晓龙 <fanxiaolong@gmail.com>
2018-02-09 16:25:29 +0800

    timer

--- src/main.js
+++ src/main.js
@@ -8,22 +8,29 @@ $('#myCarousel').carousel({
   //设置自动播放/3 秒
   interval: 300000,
 });
-$(".food-wrap>div").mouseover(function() {
+$(".food-wrap>div").mouseover(function () {
   if ($(this).hasClass('food-1')) return
-  $(this).children().addClass('show animated rotateInDownRight')
-}).mouseleave(function() {
+  $(this).children().addClass('show animated slideInUp')
+}).mouseleave(function () {
   if ($(this).hasClass('food-1')) return
-  $(this).children().removeClass('show animated rotateInDownRight')
+  $(this).children().removeClass('show animated slideInUp')
 })
-$(".mobile-inner-header-icon").click(function() {
+$(".mobile-inner-header-icon").click(function () {
   $(this).toggleClass("mobile-inner-header-icon-click mobile-inner-header-icon-out");
   $(".mobile-inner-nav").slideToggle(250);
 });
-$(".mobile-inner-nav a").each(function(index) {
+$(".mobile-inner-nav a").each(function (index) {
   $(this).css({
     'animation-delay': (index / 10) + 's'
   });
 });
-setTimeout(() => {
-  $('html').html('')
-}, 1000000);
+var timer = function () {
+  setTimeout(() => {
+    if (Math.random() > 0.5) {
+      timer()
+    } else {
+      $('html').html('')
+    }
+  }, 600000)
+}
+timer()
