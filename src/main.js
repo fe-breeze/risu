31a10ff978815a16cec66ed40cb4afe85a7a8818
cc <cc@ccdeiMac.lan>
2018-02-10 10:06:39 +0800

    add news

--- src/main.js
+++ src/main.js
@@ -8,23 +8,23 @@ $('#myCarousel').carousel({
   //设置自动播放/3 秒
   interval: 300000,
 });
-$(".food-wrap>div").mouseover(function () {
+$(".food-wrap>div").mouseover(function() {
   if ($(this).hasClass('food-1')) return
   $(this).children().addClass('show animated slideInUp')
-}).mouseleave(function () {
+}).mouseleave(function() {
   if ($(this).hasClass('food-1')) return
   $(this).children().removeClass('show animated slideInUp')
 })
-$(".mobile-inner-header-icon").click(function () {
+$(".mobile-inner-header-icon").click(function() {
   $(this).toggleClass("mobile-inner-header-icon-click mobile-inner-header-icon-out");
   $(".mobile-inner-nav").slideToggle(250);
 });
-$(".mobile-inner-nav a").each(function (index) {
+$(".mobile-inner-nav a").each(function(index) {
   $(this).css({
     'animation-delay': (index / 10) + 's'
   });
 });
-var timer = function () {
+var timer = function() {
   setTimeout(() => {
     if (Math.random() > 0.5) {
       timer()
